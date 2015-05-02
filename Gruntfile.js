01689ec52c03ee42504e8b4daf59752615cf37fd
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-02 18:41:59 +0200

    Big fucking fist commit! Hello AngularJS!

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * 
+ * This file is part of Serleena-Frontend
+ * 
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2015 Antonio Cavestro, Matteo Lisotto.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+
+module.exports = function(grunt){
+
+	var testangular_files = [
+		'./app/config/appconfiguration.js',
+		'./app/test/test.controller.js'
+	];
+
+	grunt.initConfig({
+
+		concat: {
+			options: {
+				separator: ';',
+			},
+			angular: {
+				src: testangular_files,
+				dest: './dist/testangular.js',
+			}
+		},
+		jshint: {
+			angular: {
+				src: testangular_files
+			}
+		},
+		uglify: {
+			options: {
+				mangle: false
+			},
+			frontend: {
+				files: {
+				  './dist/testangular.js': './dist/testangular.js',
+				}
+			}
+		},
+		watch: {
+			angular: {
+				files: testangular_files,
+				//tasks: ['concat:angular','uglify:frontend'],
+				tasks: ['concat:angular', 'jshint:angular', 'notify:concat'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		notify: {
+			concat: {
+				options: {
+					title: "Esito compilazione",
+					message: "Tutto ben, daghene"
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-notify');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['concat:angular', 'jshint:angular']);
+
+};
---
+++ karma-conf.js
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * 
+ * This file is part of Serleena-Frontend
+ * 
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2015 Antonio Cavestro, Matteo Lisotto.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+
+module.exports = function(config){
+    config.set({
+      basePath : './',
+
+      files : [
+        // file di Angular
+        'bower_components/angular/angular.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+
+        // file di serleena
+        'dist/testangular.js',
+
+        // test
+        'test/unit/*.js'
+      ],
+
+      autoWatch : false,
+
+      frameworks: ['jasmine'],
+
+      browsers : ['PhantomJS'],
+
+      reporters: ['progress'],
+
+      preprocessors: {
+
+      },
+
+      plugins : [
+          'karma-jasmine',
+          'karma-phantomjs-launcher'
+      ]
+})}
