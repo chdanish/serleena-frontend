553042069df035df21d4f4bdf096266fae2d0931
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-05-08 11:32:51 +0200

    Add Telemetry module
    Related to: SHFR-5

---
+++ app/telemetry/telemetry.controller.js
@@ -0,0 +1,27 @@
+/**
+   * Name: TelemetryController
+   * Package: Telemetry
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('telemetry').controller('TelemetryController',
+				       TelemetryController);
+
+/**
+  * Classe che gestisce la visualizzazione di un tracciamento di una determinata
+  * esperienza.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  * @param{scope} model che contiene i dati dell'applicazione.
+  */
+
+function TelemetryController($scope) {
+}
---
+++ app/telemetry/telemetry.service.js
@@ -0,0 +1,25 @@
+/**
+   * Name: TelemetryService
+   * Package: Telemetry
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('telemetry').service('TelemetryService', TelemetryService);
+
+/**
+  * Classe singleton che gestisce le chiamate al backend per ottenere i
+  * tracciamenti relativi a una particolare esperienza.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  */
+
+function TelemetryService() {
+}
