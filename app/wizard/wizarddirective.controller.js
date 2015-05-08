68e3bca9660f61c4293b125a3cd2f63a9c436b57
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-05-08 10:39:41 +0200

    Add Wizard module
    Related to: SHFR-5

---
+++ app/wizard/wizarddirective.controller.js
@@ -0,0 +1,25 @@
+/**
+   * Name: WizardDirectiveController
+   * Package: Wizard
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('wizard').controller('WizardDirectiveController', WizardDirectiveController);
+
+/**
+  * Classe che gestisce una generica procedura guidata.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  * @param{scope} model che contiene i dati dell'applicazione.
+  */
+
+function WizardDirectiveController($scope) {
+}
