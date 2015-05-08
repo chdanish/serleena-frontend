16edac67931ee224666627db9541ef46317e1b1b
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-05-08 11:29:38 +0200

    Add Synchronization module
    Related to: SHFR-5

---
+++ app/synchronization/pairing.controller.js
@@ -0,0 +1,25 @@
+/**
+   * Name: PairingController
+   * Package: Synchronization
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('synchronization').controller('PairingController', PairingController);
+
+/**
+  * Classe che gestisce la procedura di pairing.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  * @param{scope} model che contiene i dati dell'applicazione.
+  */
+
+function PairingController($scope) {
+}
---
+++ app/synchronization/syncexperiences.service.js
@@ -0,0 +1,23 @@
+/**
+   * Name: SyncExperiencesService
+   * Package: Synchronization
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+angular.module('synchronization').service('SyncExperiencesService', SyncExperiencesService);
+
+/**
+  * Classe singleton che gestisce le chiamate al backend relative al pairing.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  */
+
+function SyncExperiencesService() {
+}
