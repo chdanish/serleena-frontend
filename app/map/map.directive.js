81720c811fff1fa342c5242d1651bcf00daf3be3
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-05-08 10:48:56 +0200

    Add Map module
    Related to: SHFR-5

---
+++ app/map/map.directive.js
@@ -0,0 +1,24 @@
+/**
+   * Name: MapDirective
+   * Package: Map
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('map').directive('MapDirective', MapDirective);
+
+/**
+  * Classe che realizza il componente grafico di una generica mappa.
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  */
+
+function MapDirective() {
+}
---
+++ app/map/shikedata.service.js
@@ -0,0 +1,26 @@
+/**
+   * Name: SerleenaDataService
+   * Package: Map
+   * Author: Matteo Lisotto
+   * Date: 2015-05-08
+   *
+   * History:
+   * Version      Programmer       Date          Changes
+   * 0.0.1        Matteo Lisotto   2015-05-08    Create file
+   *
+   */
+
+angular.module('map').service('SerleenaDataService', SerleenaDataService);
+
+/**
+  * Classe singleton che implementa la comunicazione con il backend per
+  * interrogare la base dati su informazioni geografiche (sentieri e punti
+  * d’interesse).
+  *
+  * @author Matteo Lisotto
+  * @version 0.1
+  * @constructor
+  */
+
+function SerleenaDataService() {
+}
