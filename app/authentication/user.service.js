827feee146caab5ed6457398fd389bf77a97d84f
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-09 16:34:01 +0200

    AUTH: acavestro s'intesta UserService.
    Related to SHFR-9

--- app/authentication/user.service.js
+++ app/authentication/user.service.js
@@ -29,7 +29,7 @@
 /**
    * Name: LogoutController
    * Package: Authentication
-   * Author: Matteo Lisotto
+   * Author: Antonio Cavestro
    * Date: 2015-05-08
    *
    * History:
@@ -44,7 +44,7 @@ angular.module('authentication').service('UserService', UserService);
   * Classe singleton che implementa la comunicazione con il backend per le
   * richieste relative alla gestione utente.
   *
-  * @author Matteo Lisotto
+  * @author Antonio Cavestro
   * @version 0.1
   * @constructor
   */
