b00e10ad9130c0a23244333b699a3a7d40c8fc13
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-10 11:10:34 +0200

    AUTH: Aggiungi metodo per effetturare richieste con auth_token.
    Related to SHFR-9

--- app/authentication/auth.service.js
+++ app/authentication/auth.service.js
@@ -69,9 +69,14 @@ function AuthService($http, $cookies, BACKEND_URL) {
     return true;
   };
 
+  var authRequest = function(callback){
+    callback($cookies.get("serleena_token"));
+  };
+
   return {
     loginUser: loginUser,
     logoutUser: logoutUser,
-    isLogged: isLogged
+    isLogged: isLogged,
+    authRequest: authRequest
   };
 }
