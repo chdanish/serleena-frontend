867d08ac2aeab60bf2de3a03977a6b2fc76643ca
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-10 19:06:03 +0200

    CTRL: Aggiungi funzione di logout.
    Related to SHFR-9

--- app/authentication/logout.controller.js
+++ app/authentication/logout.controller.js
@@ -35,6 +35,7 @@
    * History:
    * Version      Programmer       Date          Changes
    * 0.0.1        Matteo Lisotto   2015-05-08    Crea file
+   * 0.0.2        Antonio Cavestro 2015-05-10    Aggiungi funzione di logout
    *
    */
 
@@ -53,4 +54,9 @@ angular.module('authentication').controller('LogoutController', LogoutController
   */
 function LogoutController($scope, $location, AuthService) {
 
+  $scope.logoutUser = function(){
+    AuthService.logoutUser(function(){
+      $location.path("#/");
+    });
+  };
 }
