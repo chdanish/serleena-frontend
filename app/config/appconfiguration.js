85fbc484cd8985711c11dffcdb4df78b3bcb1537
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-10 16:27:32 +0200

    CFG: Collega route /logout ai relativi Controller e View.
    Related to SHFR-9

--- app/config/appconfiguration.js
+++ app/config/appconfiguration.js
@@ -65,6 +65,10 @@ function AppConfiguration($routeProvider){
 			.when("/", {
 				templateUrl: 'app/authentication/login.view.html',
 				controller: 'LoginController'
+			})
+			.when("/logout", {
+				templateUrl: 'app/authentication/logout.view.html',
+				controller: 'LogoutController'
 			});
 	}();
 }
