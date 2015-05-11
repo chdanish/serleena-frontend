69de98641121709e063feb5cbbe4f38333437e77
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-11 18:59:29 +0200

    DOC: Aggiungi documentazione classe AppInit.
    Related to SHFR-9

--- app/config/appconfiguration.js
+++ app/config/appconfiguration.js
@@ -109,6 +109,20 @@ function AppConfiguration($routeProvider){
 	}();
 }
 
+/**
+ * Classe che contiene l'inizializzazione dell'applicazione, in particolare lo
+ * stato di autenticazione e il gestore degli accessi nelle varie aree.
+ *
+ * @constructor
+ * @param {Provider} $rootScope - ViewModel globale di AngularJS, usato per
+ * impostare globalmente se l'utente è autenticato o meno.
+ * @param {Provider} $location - Facade di AngularJS con il quale interagire
+ * per gestire la history del browser e gli indirizzi.
+ @ param {Service} AuthService - Gestore dell'autenticazione.
+ *
+ * @author Antonio Cavestro <antonio.cavestro@gmail.com>
+ * @version 0.1
+ */
 function AppInit($rootScope, $location, AuthService){
 	$rootScope.userLogged = AuthService.isLogged();
 }
