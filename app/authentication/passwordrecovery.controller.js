1584f69c3f920e5b5b033ce8c221c775b3e41ed0
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-11 08:04:08 +0200

    CFG: Inietta in PasswordRecoveryController oggetto UserService.
    Related to SHFR-9

--- app/authentication/passwordrecovery.controller.js
+++ app/authentication/passwordrecovery.controller.js
@@ -50,5 +50,5 @@ angular.module('authentication').controller('PasswordRecoveryController',
   * @param{scope} model che contiene i dati dell'applicazione.
   */
 
-function PasswordRecoveryController($scope) {
+function PasswordRecoveryController($scope, UserService) {
 }
