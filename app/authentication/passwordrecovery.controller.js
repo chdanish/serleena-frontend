85a645c0e952a67e3f5f213806ac83edfa580829
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-11 08:06:31 +0200

    CTRL: Aggiungi campo dati email a PwdRecovery.
    Related to SHFR-9

--- app/authentication/passwordrecovery.controller.js
+++ app/authentication/passwordrecovery.controller.js
@@ -29,4 +29,13 @@ angular.module('authentication').controller('PasswordRecoveryController',
   */
 
 function PasswordRecoveryController($scope, UserService) {
+  /**
+   * Email utente
+   *
+   * @name email
+   * @type String
+   * @memberOf PasswordRecoveryController
+   * @instance
+   */
+  $scope.email = "";
 }
