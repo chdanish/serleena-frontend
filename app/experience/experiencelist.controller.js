db6759018b857bf4e7099e78c0f4251ebb76187f
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-13 11:54:15 +0200

    CTRL: Aggiungi recupero lista esperienze e inizializzazione $scope.
    Ipoteticamente si può pensare come a del codice del costruttore, ma
    Javascript rende il concetto più fumoso e vago.
    
    Related to SHFR-11

--- app/experience/experiencelist.controller.js
+++ app/experience/experiencelist.controller.js
@@ -6,6 +6,8 @@
    * History:
    * Version      Programmer       Changes
    * 0.0.1        Matteo Lisotto   Create file
+   * 0.0.2        Antonio Cavestro Inserito codice per il recupero delle
+   *                               esperienze e il popolamento della vista
    *
    */
 angular.module('experience').controller('ExperienceListController',
@@ -27,4 +29,65 @@ angular.module('experience').controller('ExperienceListController',
 
 
 function ExperienceListController($scope, ExperienceService) {
+
+  ExperienceService.getExperienceList(function(ok, data){
+    if(ok){
+      /**
+       * Array contenente le esperienze.
+       *
+       * @name experiences
+       * @type Array
+       * @memberOf ExperienceListController
+       * @instance
+       */
+      $scope.experiences = data;
+      /**
+       * Numero di esperienze caricate.
+       *
+       * @name numExperiences
+       * @type Number
+       * @memberOf ExperienceListController
+       * @instance
+       */
+      $scope.numExperiences = $scope.experiences.length;
+      /**
+       * Flag che indica o meno la presenza di esperienze caricate.
+       *
+       * @name noExperiences
+       * @type Boolean
+       * @memberOf ExperienceListController
+       * @instance
+       */
+      $scope.noExperiences = $scope.numExperiences === 0;
+    } else {
+      /**
+       * Flag che indica o meno un errore di comunicazione avvenuto su
+       * ExperienceService.
+       *
+       * @name error
+       * @type Boolean
+       * @memberOf ExperienceListController
+       * @instance
+       */
+      $scope.error = true;
+      /**
+       * Eventuale messaggio di errore da parte di ExperienceService.
+       *
+       * @name errorMsg
+       * @type String
+       * @memberOf ExperienceListController
+       * @instance
+       */
+      $scope.errorMsg = "Errore di comunicazione con il server :(";
+    }
+    /**
+     * Flag che abilita la visualizzazione di alert
+     *
+     * @name showAlert
+     * @type Boolean
+     * @memberOf ExperienceListController
+     * @instance
+     */
+    $scope.showAlert = $scope.noExperiences || $scope.error;
+  });
 }
