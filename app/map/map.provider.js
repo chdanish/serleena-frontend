bd05cc82ffa6c4820e1be9afbbd769955b2cba36
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-14 11:03:28 +0200

    MAP: Aggiungi prima implementazione di MapProvider.

--- app/map/map.provider.js
+++ app/map/map.provider.js
@@ -29,25 +29,39 @@
 /**
    * Name: MapProvider
    * Package: Map
-   * Author: Matteo Lisotto
+   * Author: Antonio Cavestro
    *
    * History:
-   * Version      Programmer         Changes
-   * 0.0.1        Matteo Lisotto     Create file
+   * Version      Programmer       Changes
+   * 0.0.1        Matteo Lisotto   Create file
    *
    */
 
-angular.module('map').service('MapProvider', MapProvider);
+angular.module('map').provider('Map', MapProvider);
 
 /**
   * Classe singleton che realizza un’astrazione del servizio con cui interagire
   * con il gestore alla mappa, in modo da disaccoppiarlo dal resto
   * dell’applicazione.
   *
-  * @author Matteo Lisotto
+  * @author Antonio Cavestro
   * @version 0.1
   * @constructor
   */
 
 function MapProvider() {
+  var mapType = "GoogleMaps";
+
+  this.setMapType = function(map){
+    mapType = map;
+  };
+
+  this.$get = function(){
+    if(mapType == "GoogleMaps"){
+      return new GoogleMapsService();
+    } else {
+      return null;
+    }
+  };
+
 }
