f47b691bb156f985f9a17870466d62198b77141a
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-14 11:20:59 +0200

    APPCFG: Inietta MapProvider.
    Related to SHFR-18

--- app/config/appconfiguration.js
+++ app/config/appconfiguration.js
@@ -52,7 +52,7 @@
  * @author Antonio Cavestro <antonio.cavestro@gmail.com>
  * @version 0.1
  */
-function AppConfiguration($routeProvider){
+function AppConfiguration($routeProvider, MapProvider){
 	/**
 	 * Configura le route dell'applicazione.
 	 * @private
