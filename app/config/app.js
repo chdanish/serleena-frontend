81948acc11f9a07886cf8a51ad5377ed309bf5ae
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-14 11:19:09 +0200

    CFG: Sposta dichiarazione dell'applicazione in file a parte.
    Related to SHFR-18

--- app/config/app.js
+++ app/config/app.js
@@ -27,65 +27,38 @@
 
 
 /**
- * Name: appconfiguration.js
+ * Name: app.js
  * Package: App
  * Author: Antonio Cavestro
  *
  * History:
  * Version    Programmer  		Changes
- * 0.0.1      Antonio Cavestro 	Crea file
- * 0.0.2      Matteo Lisotto	Aggiungi moduli e aggiorna dipendenze di
- *								serleenaFrontend
+ * 0.0.1      Antonio Cavestro 	2015-05-06  Crea file
  *
  */
+angular
+        .module('authentication', ['ngCookies']);
+angular
+        .module('experience', ['wizard']);
+angular
+        .module('map', []);
+angular
+        .module('synchronization', []);
+angular
+        .module('telemetry', []);
+angular
+        .module('wizard', []);
 
-/**
- * @namespace Configuration
- */
+angular
+	.module('serleenaFrontend', [
+		'ngRoute',
+	        'authentication',
+	        'experience',
+	        'map',
+	        'synchronization',
+	        'telemetry',
+	]).config(AppConfiguration)
+	.run(AppInit);
 
-/**
- * Classe che contiene la configurazione dell'applicazione.
- *
- * @constructor
- * @param {Provider} $routeProvider - Il gestore delle route di AngularJS
- *
- * @author Antonio Cavestro <antonio.cavestro@gmail.com>
- * @version 0.1
- */
-function AppConfiguration($routeProvider){
-	/**
-	 * Configura le route dell'applicazione.
-	 * @private
-	 * @function setRoutes
-	 * @memberOf AppConfiguration
-	 * @instance
-	 */
-	var setRoutes = function(){
-		$routeProvider
-			.when("/", {
-				templateUrl: 'app/authentication/login.view.html',
-				controller: 'LoginController'
-			})
-			.when("/logout", {
-				templateUrl: 'app/authentication/logout.view.html',
-				controller: 'LogoutController'
-			})
-			.when("/recoverpassword", {
-				templateUrl: 'app/authentication/passwordrecovery.view.html',
-				controller: 'PasswordRecoveryController'
-			})
-			.when("/register", {
-				templateUrl: 'app/authentication/register.view.html',
-				controller: 'RegisterController'
-			})
-			.when("/dashboard", {
-				templateUrl: 'app/experience/experiencelist.view.html',
-				controller: 'ExperienceListController'
-			})
-			.when("/dashboard/experiences/new", {
-				templateUrl: 'app/experience/experiencewizard.view.html',
-				controller: 'ExperienceWizardController'
-			});
-	}();
-
-}
+angular.module('serleenaFrontend')
+	.value('BACKEND_URL', 'http://api.hitchhikers.info');
