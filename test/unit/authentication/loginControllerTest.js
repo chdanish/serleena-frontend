a5d8472ea9e3f760b1d16d5ec52b3ad1a4a16efb
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-05-15 15:12:16 +0200

    TEST: Aggiungi unit test di loginController
    Related to: SHFR-10

---
+++ test/unit/authentication/loginControllerTest.js
@@ -0,0 +1,71 @@
+/**
+  * Name: LoginControllerTest
+  * Package: Authentication
+  * Author: Matteo Lisotto <matteo.lisotto@gmail.com>
+  *
+  * History
+  * Version    Programmer        Changes
+  * 1.0.0      Matteo Lisotto    Crea file e test per LoginController
+  *
+  */
+
+describe('LoginController Test', function () {
+    var $scope, location, authService;
+
+    beforeEach(function() {
+	angular.mock.module('ngCookies');
+	module('authentication');
+    });
+
+    beforeEach(inject(function($controller) {
+	$scope = {};
+
+	location = jasmine.createSpyObj('location', ['path']);
+
+	authService = jasmine.createSpyObj('AuthService', ['loginUser']);
+	authService.loginUser.and.callFake(function (email, pwd) {
+	    if (email == "test@test.it" && pwd == "pwd")
+		location.path("/dashboard");
+	    else {
+		$scope.errorMessage = "Accesso fallito :(";
+		$scope.showError = true;
+	    }
+	});
+	
+	$controller('LoginController', {
+	    $scope: $scope,
+	    $location: location,
+	    AuthService: authService
+	});
+    }));
+
+    
+    it('Successfully login', function () {
+	$scope.email = "test@test.it";
+	$scope.password = "pwd";
+	$scope.loginUser();
+
+	expect($scope.email).toBeDefined();
+	expect($scope.password).toBeDefined();
+
+	expect($scope.email).toBe("test@test.it");
+	expect($scope.password).toBe("pwd");
+	expect(location.path).toHaveBeenCalledWith("/dashboard");
+    });
+
+    it('Wrong login', function () {
+	$scope.email = "wrong@test.it";
+	$scope.password = "pwd";
+	$scope.loginUser();
+
+	expect($scope.email).toBeDefined();
+	expect($scope.password).toBeDefined();
+	expect($scope.errorMessage).toBeDefined();
+	expect($scope.showError).toBeDefined();
+	
+	expect($scope.email).toBe("wrong@test.it");
+	expect($scope.password).toBe("pwd");
+	expect($scope.errorMessage).toBe("Accesso fallito :(");
+	expect($scope.showError).toBe(true);
+    });
+});
