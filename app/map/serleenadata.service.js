d1d435278fa04a597851fc084abd26d927bfd8b5
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-16 09:47:57 +0200

    SHDATA: Aggiungi metodo a SerleenaDataService per ottenere i sentieri.
    Related to SHFR-18

--- app/map/serleenadata.service.js
+++ app/map/serleenadata.service.js
@@ -54,4 +54,30 @@ angular.module('map').service('SerleenaDataService', SerleenaDataService);
   */
 
 function SerleenaDataService($http, BACKEND_URL) {
+  /**
+   * Metodo per ottenere la lista dei sentieri in un determinato perimetro.
+   *
+   * @function getPaths
+   * @memberOf SerleenaDataService
+   * @instance
+   * @param {Object} from - oggetto che rappresenta il punto a nord-est
+   * dell'area di cui si vuole conoscere i sentieri, con un attributo "lat" per
+   * la latitudine e un attributo "lng" per la longitudine.
+   * @param {Object} to - oggetto che rappresenta il punto a sud-ovest
+   * dell'area di cui si vuole conoscere i sentieri, con un attributo "lat" per
+   * la latitudine e un attributo "lng" per la longitudine.
+   * @param {function} callback - funzione da invocare alla ricezione della
+   * risposta dal backend e a cui passare i dati ricevuti.
+   */
+  var getPaths = function(from, to, callback){
+    $http({
+      url: BACKEND_URL + "/paths/" + from.lat + ";" + from.lng + "/" + to.lat +
+                                                                  ";" + to.lng,
+      method: 'GET'
+    }).success(function(data){
+      callback(true, data.paths);
+    }).error(function(data){
+      callback(false, data);
+    });
+  };
 }
