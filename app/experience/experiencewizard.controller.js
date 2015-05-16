0d9df111f8a89cc0df4cd99698994d7fbb40839e
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-16 11:39:34 +0200

    SCOPE: Aggiungi var per visualizzare la grafica di inserimento percorsi.
    Related to SHFR-18

--- app/experience/experiencewizard.controller.js
+++ app/experience/experiencewizard.controller.js
@@ -74,6 +74,16 @@ function ExperienceWizardController($scope, Map, SerleenaDataService) {
    */
   $scope.mapTagId = "";
   /**
+   * Flag per la visualizzazione delle informazioni relativi ai percorsi.
+   *
+   * @name showTracks
+   * @type Boolean
+   * @default false
+   * @memberOf ExperienceWizardController
+   * @instance
+   */
+  $scope.showTracks = false;
+  /**
    * Gestisce l'evento hhMapLink lanciato da MapDirective, in modo da poter
    * ottenere l'Id di quest'ultima.
    *
@@ -122,6 +132,7 @@ function ExperienceWizardController($scope, Map, SerleenaDataService) {
           });
         }
       });
+    $scope.showTracks = true;
   };
   $scope.$on('hhMapLink', linkMap);
 }
