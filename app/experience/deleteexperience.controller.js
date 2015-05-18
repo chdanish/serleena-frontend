74dbc7396ff1986af4fad1a67e5f76f3586746fe
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-18 16:45:23 +0200

    DELEXP: Aggiungi struttura classe DeleteExperienceController.
    Related to SHFR-15

---
+++ app/experience/deleteexperience.controller.js
@@ -0,0 +1,28 @@
+/**
+   * Name: DeleteExperienceController
+   * Package: Experience
+   * Author: Antonio Cavestro
+   *
+   * History:
+   * Version      Programmer          Changes
+   * 0.0.1        Antonio Cavestro    Create file
+   *
+   */
+
+angular.module("experience").controller("DeleteExperienceController",
+	DeleteExperienceController);
+
+/**
+  * Classe che gestisce la cancellazione di un'esperienza.
+  *
+  * @author Antonio Cavestro
+  * @version 0.1
+  * @constructor
+  * @param {Scope} $scope - L'oggetto ViewModel del controller.
+  * @param {Service} $routeParams - Service che gestisce il recupero dei
+  * parametri passati via url.
+  */
+
+function DeleteExperienceController($scope, $routeParams){
+
+}
