c9578f0c8a41f5221ca9185dc5ccd4c7c1839998
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-19 07:14:34 +0200

    SYNCCTRL: Aggiungi array per lista di sincronizzazione.
    Related to SHFR-21

--- app/synchronization/syncexperiences.controller.js
+++ app/synchronization/syncexperiences.controller.js
@@ -25,4 +25,14 @@ angular.module('synchronization').controller('SyncExperiencesController',
   */
 
 function SyncExperiencesController($scope, SyncExperiencesService) {
+  /**
+   * Array contenente le esperienze con le informazioni relative all'effettiva
+   * o meno abilitazione alla sincronizzazione.
+   *
+   * @name experiences
+   * @type Array
+   * @memberOf SyncExperiencesController
+   * @instance
+   */
+  $scope.experiences = [];
 }
