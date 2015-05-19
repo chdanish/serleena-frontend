9aae1bf13e884c6821979d5b21acef15511ecfe4
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-19 06:47:44 +0200

    CFG: Correggi errore indentazione codice.
    Related to SHFR-21

--- app/config/appconfiguration.js
+++ app/config/appconfiguration.js
@@ -58,8 +58,8 @@ function AppConfiguration($routeProvider, MapProvider){
 			.when("/dashboard/experiences/new", {
 				templateUrl: 'app/experience/experiencewizard.view.html',
 				controller: 'ExperienceWizardController'
-			}).
-			when("/dashboard/experiences/:experienceId/delete", {
+			})
+			.when("/dashboard/experiences/:experienceId/delete", {
 				templateUrl: 'app/experience/deleteexperience.view.html',
 				controller: 'DeleteExperienceController'
 			})
