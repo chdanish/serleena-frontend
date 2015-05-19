0bfebdce68786809b3facc2a86c9bf3d36873874
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-19 11:24:59 +0200

    REGCTRL: Aggiungi dipendenza verso AuthService.
    Related to SHFR-20

--- app/authentication/register.controller.js
+++ app/authentication/register.controller.js
@@ -52,9 +52,10 @@ angular.module('authentication').controller('RegisterController', RegisterContro
   * @constructor
   * @param {Scope} $scope - L'oggetto ViewModel del controller.
   * @param {Service} UserService - Servizio che gestisce le informazioni utente.
+  * @param {Service} AuthService - Servizio che gestisce l'autenticazione utente.
   */
 
-function RegisterController($scope, UserService) {
+function RegisterController($scope, UserService, AuthService) {
   /**
    * Email utente
    *
