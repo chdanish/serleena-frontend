71274ae5fc04a0733715ed70b5dc56995ac6b0ad
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-19 12:00:18 +0200

    PAIRCTRL: Aggingi attributo per salvare token temporaneo.
    Related to SHFR-20

--- app/synchronization/pairing.controller.js
+++ app/synchronization/pairing.controller.js
@@ -23,4 +23,13 @@ angular.module('synchronization').controller('PairingController', PairingControl
   */
 
 function PairingController($scope, PairingService) {
+  /**
+   * Token temporaneo
+   *
+   * @name tempToken
+   * @type String
+   * @memberOf PairingController
+   * @instance
+   */
+  $scope.tempToken = "";
 }
