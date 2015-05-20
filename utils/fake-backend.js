dcee8b3f6f216ff128d0797b07c9c0852380a39c
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-20 12:39:07 +0200

    FAKEBACKEND: Aggiungi supporto route dei percorsi.
    Related to SHFR-25

--- utils/fake-backend.js
+++ utils/fake-backend.js
@@ -233,6 +233,44 @@ app.get('/experiences/:id', function(req, res){
   res.send(JSON.stringify(exp));
 });
 
+app.get('/experiences/:eid/tracks/:tid', function(req, res){
+  var c = [
+    {
+      lat: 45.276413,
+      lng: 11.650587
+    },
+    {
+      lat: 45.275977,
+      lng: 11.652806
+    },
+    {
+      lat: 45.276257,
+      lng: 11.654908
+    },
+    {
+      lat: 45.277573,
+      lng: 11.654297
+    },
+    {
+      lat: 45.278192,
+      lng: 11.655213
+    },
+    {
+      lat: 45.279032,
+      lng: 11.655626
+    },
+    {
+      lat: 45.280266,
+      lng: 11.655875
+    },
+    {
+      lat: 45.281726,
+      lng: 11.655550
+    }
+  ];
+  res.send(c);
+});
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
