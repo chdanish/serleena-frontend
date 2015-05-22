0dc712bc9e21ddb5180becc71ac6924252700763
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-22 16:47:30 +0200

    TELCTRL: Aggiungi attributo con nome esperienza.
    Related to SHFR-22

--- app/telemetry/telemetry.controller.js
+++ app/telemetry/telemetry.controller.js
@@ -76,4 +76,13 @@ function TelemetryController($scope, ExperienceService, TelemetryService,
    * @instance
    */
   $scope.trackId = $routeParams.trackId;
+  /**
+   * Nome dell'esperienza di cui si stanno visualizzando i tracciamenti.
+   *
+   * @name expName
+   * @type String
+   * @memberOf TelemetryController
+   * @instance
+   */
+  $scope.expName = "";
 }
