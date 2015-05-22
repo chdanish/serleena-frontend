636e182d885bb295f4c1a231a9154df3b81b3ceb
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-22 16:31:42 +0200

    TELSRV: Intesta classe ad acavestro.
    Related to SHFR-22

--- app/telemetry/telemetry.service.js
+++ app/telemetry/telemetry.service.js
@@ -29,7 +29,7 @@
 /**
    * Name: TelemetryService
    * Package: Telemetry
-   * Author: Matteo 
+   * Author: Antonio Cavestro
    *
    * History:
    * Version      Programmer           Changes
@@ -43,7 +43,7 @@ angular.module('telemetry').service('TelemetryService', TelemetryService);
   * Classe singleton che gestisce le chiamate al backend per ottenere i
   * tracciamenti relativi a una particolare esperienza.
   *
-  * @author Matteo Lisotto
+  * @author Antonio Cavestro
   * @version 0.1
   * @constructor
   */
