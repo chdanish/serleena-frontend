bd3a0b623d60b8e9dd8b92dea330e05d6e38281c
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-26 18:28:44 +0200

    DOC: s/@use/@example/g in pkg Telemetry.
    Related to SHFR-52

--- app/telemetry/telemetry.controller.js
+++ app/telemetry/telemetry.controller.js
@@ -21,7 +21,7 @@ angular.module('telemetry').controller('TelemetryController',
   *
   * @author Antonio Cavestro
   * @version 0.1
-  * @use L’applicativo è configurato tramite la classe App.AppConfiguration per
+  * @example L’applicativo è configurato tramite la classe App.AppConfiguration per
   * invocare questo controller quando il browser punta all’indirizzo della
   * pagina di visualizzazione dei tracciamenti. Carica i dati invocando
   * TelemetryService, passandogli come parametro il codice identificativo
