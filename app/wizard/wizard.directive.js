0b443ecab7846c2bd5b3935d95c4bbc8013bb469
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-05-26 15:22:15 +0200

    DOC: Aggiungi documentazione WizardDirective.
    Related to SHFR-48

--- app/wizard/wizard.directive.js
+++ app/wizard/wizard.directive.js
@@ -50,11 +50,53 @@ angular.module('wizard').directive('hhWizard', WizardDirective);
 
 function WizardDirective() {
   var directive = {
+    /**
+     * Variabile che permette di limitare l'uso della directive in un
+     * particolare tipo di entità del DOM, a scelta tra attributo, elemento o
+     * classe.
+     *
+     * @name restrict
+     * @type String
+     * @default E
+     * @memberOf Wizard.WizardDirective
+     * @instance
+     */
     restrict: 'E',
+    /**
+     * Nome del controller che gestisce la directive.
+     *
+     * @name controller
+     * @type String
+     * @memberOf Wizard.WizardDirective
+     * @instance
+     */
     controller: 'WizardDirectiveController',
     transclude: true,
     replace: true,
+    /**
+     * URL della pagina HTML separata che andrà a rimpiazzare il codice della
+     * directive.
+     *
+     * @name templateUrl
+     * @type String
+     * @memberOf Wizard.WizardDirective
+     * @instance
+     */
     templateUrl: 'app/wizard/wizarddirective.view.html',
+    /**
+       * Funzione invocata da AngularJS per inizializzare la directive. Essa,
+       * in questo caso, attiva il primo step del wizard.
+       *
+       * @function link
+       * @memberOf Wizard.WizardDirective
+       * @instance
+       * @param {Scope} scope - Scope accessibile dalla directive. In questo
+       * caso quello del controller che la gestisce.
+       * @param {DOMElement} iElement - Oggetto che rappresenta la directive
+       * all'interno del DOM della pagina.
+       * @param {Object} iAttrs - Oggetto che contiene gli attributi della
+       * directive, nel formato chiave-valore.
+      */
     link: function (scope, iElement, iAttrs) {
       scope.steps[scope.currentStepIndex].currentStep = true;
     }
