1fcd38e6031f43c1d1956f054cb1c705eb628709
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-08-03 23:42:12 +0200

    PROXY: Aggiungi route GET /experiences/:eid/tracks/:tid.
    Related to SHFR-69

--- utils/marvinproxy.js
+++ utils/marvinproxy.js
@@ -213,6 +213,10 @@ app.get('/experiences/:experienceId', function (req, res) {
   proxyRequest('get', '/experiences', ['X-AuthToken'], req, res);
 });
 
+app.get('/experiences/:experienceId/tracks/:trackId', function (req, res) {
+  proxyRequest('get', '/experiences+/tracks', ['X-AuthToken'], req, res);
+});
+
 var server = app.listen(4242, function () {
 
   var host = server.address().address;
